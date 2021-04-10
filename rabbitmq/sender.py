64348874e01cc911e0aaf51a789a0fc6c961323a
KivutiBrian <briankivuti1@gmail.com>
2021-04-10 12:51:22 +0300

    rabbitmq sender logs 5

--- rabbitmq/sender.py
+++ rabbitmq/sender.py
@@ -3,12 +3,13 @@ import pika
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
-channel.queue_declare(queue='duka-sales-queue')
+credentials = pika.PlainCredentials('guest', 'guest')
+channel.queue_declare(queue='duka-inv-queue', credentials)
 
 def send_log_to_queue(message: str):
     channel.basic_publish(
         exchange='',
-        routing_key='duka-sales-queue',
+        routing_key='duka-inv-queue',
         body=message
     )
 
