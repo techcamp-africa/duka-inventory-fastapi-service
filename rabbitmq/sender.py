c907f2c24084458ba8346d1280aa0801b9b5b65f
KivutiBrian <briankivuti1@gmail.com>
2021-04-11 10:11:31 +0300

    rabbitmq sender logs 11

--- rabbitmq/sender.py
+++ rabbitmq/sender.py
@@ -2,8 +2,7 @@ import pika
 
 credentials = pika.PlainCredentials('guest', 'guest')
 
-# connection = pika.BlockingConnection(pika.ConnectionParameters('138.68.189.32',5672,'/',credentials))
-connection = pika.BaseConnection(pika.ConnectionParameters('localhost',5672,'/',credentials))
+connection = pika.BlockingConnection(pika.ConnectionParameters('138.68.189.32',5672,'/'))
 channel = connection.channel()
 
 channel.queue_declare(queue='duka-inv-queue')
